ba89796aa871ed2ba465a45530cc428f9d5825be
dan437 <80175477+dan437@users.noreply.github.com>
2021-09-29 14:59:57 +0200

    Use our new transaction APIs server URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://api2.metaswap-st.codefi.network';
+export const API_BASE_URL = 'https://transaction.metaswap-dev.codefi.network';
 export const CHAIN_IDS = {
   ETHEREUM: '1', // 0x1 in hex
   BSC: '56', // 0x38 in hex
