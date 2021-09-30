d86bd6a199874ec32cdc5cc4b01952cf813852d6
dan437 <80175477+dan437@users.noreply.github.com>
2021-09-30 13:58:12 +0200

    Use chainId in DEC for API calls

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
-export const API_BASE_URL = 'https://transaction.metaswap-dev.codefi.network';
+export const API_BASE_URL = 'http://localhost:4000';
 export const CHAIN_IDS = {
-  ETHEREUM: '1', // 0x1 in hex
-  BSC: '56', // 0x38 in hex
+  ETHEREUM: '0x1', // 1 in dec
+  RINKEBY: '0x4', // 4 in dec
+  BSC: '0x38', // 56 in dec
 };
+export const CHAIN_IDS_HEX_TO_DEC = {
+  '0x1': '1',
+  '0x4': '4',
+  '0x38': '56',
+} as any;
