092cfea84ceccde2e743200a229289d2a07cb2f9
dan437 <80175477+dan437@users.noreply.github.com>
2021-10-07 15:54:29 +0200

    Don't call polling when check an instance of the SmartTransactionsController

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,11 +3,17 @@ import DefaultExport from '.';
 
 describe('default export', () => {
   it('exports SmartTransactionsController', () => {
+    const unsupportedChainId = '99999';
     expect(
-      new DefaultExport({
-        onNetworkStateChange: jest.fn(),
-        nonceTracker: jest.fn(),
-      }),
+      new DefaultExport(
+        {
+          onNetworkStateChange: jest.fn(),
+          nonceTracker: null,
+        },
+        {
+          chainId: unsupportedChainId, // This is needed so our code wouldn't do polling in this test.
+        },
+      ),
     ).toBeInstanceOf(SmartTransactionsController);
   });
 });
