04d87b61d15447fff9a6bd4c7bc27953f44b3931
dan437 <80175477+dan437@users.noreply.github.com>
2021-10-07 14:10:37 +0200

    Update the CHAIN_IDS_HEX_TO_DEC constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
 export const API_BASE_URL = 'https://transaction.metaswap-dev.codefi.network';
 export const CHAIN_IDS = {
-  ETHEREUM: '0x1', // 1 in dec
-  RINKEBY: '0x4', // 4 in dec
-  BSC: '0x38', // 56 in dec
+  ETHEREUM: '0x1',
+  RINKEBY: '0x4',
+  BSC: '0x38',
 };
 export const CHAIN_IDS_HEX_TO_DEC = {
-  '0x1': '1',
-  '0x4': '4',
-  '0x38': '56',
+  [CHAIN_IDS.ETHEREUM]: '1',
+  [CHAIN_IDS.RINKEBY]: '4',
+  [CHAIN_IDS.BSC]: '56',
 } as any;
