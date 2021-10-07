12a48b3f43595b6e68bd34b43af2cc318058e1ef
dan437 <80175477+dan437@users.noreply.github.com>
2021-10-07 16:30:25 +0200

    Use "jest.useFakeTimers" to avoid long wait for setTimeout

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,17 +3,12 @@ import DefaultExport from '.';
 
 describe('default export', () => {
   it('exports SmartTransactionsController', () => {
-    const unsupportedChainId = '99999';
-    expect(
-      new DefaultExport(
-        {
-          onNetworkStateChange: jest.fn(),
-          nonceTracker: null,
-        },
-        {
-          chainId: unsupportedChainId, // This is needed so our code wouldn't do polling in this test.
-        },
-      ),
-    ).toBeInstanceOf(SmartTransactionsController);
+    jest.useFakeTimers();
+    const controller = new DefaultExport({
+      onNetworkStateChange: jest.fn(),
+      nonceTracker: null,
+    });
+    expect(controller).toBeInstanceOf(SmartTransactionsController);
+    jest.clearAllTimers();
   });
 });
