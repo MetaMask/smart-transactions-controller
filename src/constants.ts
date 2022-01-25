2363d1b9b788e8d1a9a8af3e29959e40dc602de6
Matthew Epps <mepps32@gmail.com>
2022-01-25 15:17:51 -0800

    undo constant changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-// export const API_BASE_URL = 'https://transaction.metaswap.codefi.network';
-export const API_BASE_URL = 'http://localhost:4000';
+export const API_BASE_URL = 'https://transaction.metaswap.codefi.network';
 export const CHAIN_IDS = {
   ETHEREUM: '0x1',
   RINKEBY: '0x4',
