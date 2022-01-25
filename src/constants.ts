cf6f96c8500e7fcf4e2756e09672bba676391b7a
Matthew Epps <mepps32@gmail.com>
2022-01-25 15:17:06 -0800

    Estimate approval tx in addition to swaps tx

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'https://transaction.metaswap.codefi.network';
+// export const API_BASE_URL = 'https://transaction.metaswap.codefi.network';
+export const API_BASE_URL = 'http://localhost:4000';
 export const CHAIN_IDS = {
   ETHEREUM: '0x1',
   RINKEBY: '0x4',
