b5910462c5089a5e293b4575a3873f12450fd45a
Alex <adonesky@gmail.com>
2023-10-16 11:29:37 -0500

    fix type issue

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,12 @@
+import { Hex } from './types';
+
 export const API_BASE_URL = 'https://transaction.metaswap.codefi.network';
-export const CHAIN_IDS = {
+export const CHAIN_IDS: {
+  ETHEREUM: Hex;
+  GOERLI: Hex;
+  RINKEBY: Hex;
+  BSC: Hex;
+} = {
   ETHEREUM: '0x1',
   GOERLI: '0x5',
   RINKEBY: '0x4',
