8346ac4b062e9671e55b341a1be41aa73625b51b
IF <139582705+infiniteflower@users.noreply.github.com>
2024-04-11 15:56:21 -0400

    chore: properly commit dist

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const SmartTransactionsController_1 = __importDefault(require("./SmartTransactionsController"));
+exports.default = SmartTransactionsController_1.default;
+//# sourceMappingURL=index.js.map
